b35307ba7f36ba598dc0248a06590776919e8c0b
Gelosu <118784451+Gelosu@users.noreply.github.com>
2023-07-18 10:42:51 +0800

    Add files via upload

---
+++ page.js
@@ -0,0 +1,11 @@
+
+export default function MAIN() {
+  return (
+    <div className="MAIN">
+        <h3>MAIN PAGE</h3>
+        <a href="/register">REGISTER HERE</a>
+        <div></div>
+        <a href="/login">LOGIN HERE</a>
+    </div>
+  )
+}
